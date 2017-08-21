f8b03c8b897256291db5348cb9fdc5f2b86bbc07
Keyvan Hadjari <keyvan.hadjari@megger.com>
2017-08-21 09:12:48 +0200

    add cli

--- angular/GettingStarted/APM/src/app/app.module.ts
+++ angular/GettingStarted/APM/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent }  from './app.component';
 import { ProductListComponent} from './products/product-list.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 import { StarComponent } from "./shared/star.component";
+import { ProductDetailComponent } from './products/product-detail.component';
 
 @NgModule({
   imports: [ 
@@ -17,7 +18,8 @@ import { StarComponent } from "./shared/star.component";
     AppComponent, 
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent],
+    StarComponent,
+    ProductDetailComponent],
   providers: [],
   bootstrap: [ 
     AppComponent]  
